566b06708bbb625c615ccb4df39cf4a634e07250
aMediocreDad <amediocredad@outlook.com>
2023-05-27 18:25:34 +0200

    test: Better test cleanup

--- __tests__/action.test.ts
+++ __tests__/action.test.ts
@@ -1,6 +1,6 @@
-import { describe, it, expect, beforeAll } from "vitest";
+import { describe, it, expect, beforeAll, beforeEach, afterAll } from "vitest";
 import { Package } from "@foundryvtt/foundryvtt-cli";
-import { mkdir, readFile } from "node:fs/promises";
+import { mkdir, readFile, rm, rmdir } from "node:fs/promises";
 import { existsSync } from "node:fs";
 
 const jsonDir = new URL("../__fixtures__/dummy-json/test", import.meta.url).pathname;
@@ -9,12 +9,22 @@ const packDir = new URL("../__fixtures__/dummy-module/packs/dummy", import.meta.
 const outDir = new URL("../__test-files__", import.meta.url).pathname;
 
 beforeAll(async () => {
-	if (!existsSync(outDir)) {
-		await mkdir(outDir, { recursive: true });
-	}
+	if (existsSync(outDir)) await rmdir(outDir, { recursive: true });
+	await mkdir(outDir, { recursive: true });
+});
+
+afterAll(async () => {
+	await rmdir(outDir, { recursive: true });
+	if (existsSync(`${moduleDir}/test`)) await rmdir(`${moduleDir}/test`, { recursive: true });
+	if (existsSync(`${moduleDir}/test.db`)) await rm(`${moduleDir}/test.db`);
 });
 
 describe("@FoundryVTT/fvtt-cli:Package", () => {
+	beforeEach(async () => {
+		if (existsSync(`${moduleDir}/test`)) await rmdir(`${moduleDir}/test`, { recursive: true });
+		if (existsSync(`${moduleDir}/test.db`)) await rm(`${moduleDir}/test.db`);
+	});
+
 	it("should unpack a Classic LevelDB as JSON", async () => {
 		await Package.unpackClassicLevel(packDir, outDir);
 		expect(existsSync(`${outDir}/test_2_yeEHA8lgkCbNr4V7.json`)).toBe(true);
@@ -23,10 +33,12 @@ describe("@FoundryVTT/fvtt-cli:Package", () => {
 		expect(json).toHaveProperty("name", "Test4");
 		expect(json).toHaveProperty("_key", "!items!f6ViPysunpvdWxq9");
 	});
+
 	it("should produce a valid Classic levelDB structure", async () => {
 		await Package.packClassicLevel(moduleDir + "/test", jsonDir);
 		expect(existsSync(`${moduleDir}/test/LOCK`)).toBe(true);
 	});
+
 	it("should produce a valid nedb database file", async () => {
 		await Package.packNedb(moduleDir, jsonDir, "test");
 		expect(existsSync(`${moduleDir}/test.db`)).toBe(true);
